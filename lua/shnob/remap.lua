06802569f4917a430a1da466843930e719050700
Shnob <jake.dombnz@gmail.com>
2023-03-27 12:48:09 +1300

    Added tab mappings

--- lua/shnob/remap.lua
+++ lua/shnob/remap.lua
@@ -8,3 +8,10 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 vim.keymap.set("n", "n", "nzz")
 vim.keymap.set("n", "N", "nzz")
+
+vim.keymap.set("n", "<leader>T", vim.cmd.terminal)
+
+vim.keymap.set("n", "<leader>tt", vim.cmd.tabnew)
+vim.keymap.set("n", "<leader>tn", vim.cmd.tabn)
+vim.keymap.set("n", "<leader>tp", vim.cmd.tabp)
+vim.keymap.set("n", "<leader>tc", vim.cmd.tabclose)
