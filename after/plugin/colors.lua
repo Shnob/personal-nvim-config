bbc90115587c6f57c30449648958a9d9266ad8f9
Shnob <jake.dombnz@gmail.com>
2023-04-13 19:57:01 +1200

    Changed background colour of active buffer

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -3,8 +3,8 @@ function ColorEditor(color)
 	color = color or "tokyonight"
 	vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-	vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+	vim.api.nvim_set_hl(0, "Normal", {bg = "#1a1b26"})
+	vim.api.nvim_set_hl(0, "NormalFloat", {bg = "#1a1b26"})
 end
 
 vim.cmd[[colorscheme tokyonight-night]]
