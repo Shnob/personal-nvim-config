9c1fa77591b903a13535c2f403c021d5ec475e49
Shnob <jake.dombnz@gmail.com>
2023-04-13 23:27:28 +1200

    Added vim-airline-themes

--- lua/shnob/packer.lua
+++ lua/shnob/packer.lua
@@ -27,8 +27,8 @@ return require('packer').startup(function(use)
 
     use('tpope/vim-fugitive')
 
-    --use('vim-airline/vim-airline')
-    use('nvim-lualine/lualine.nvim')
+    use('vim-airline/vim-airline')
+    use('vim-airline/vim-airline-themes')
 
     use {
         'VonHeikemen/lsp-zero.nvim',
