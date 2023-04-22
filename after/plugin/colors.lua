d1af729086881ac43b6e74880fe46abe78d69d5b
Shnob <jake.dombnz@gmail.com>
2023-04-22 23:44:19 +1200

    Fixed colourscheme, is now tokyonight-night

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,11 +1,10 @@
 
 function ColorEditor(color)
-	color = color or "tokyonight"
+	color = color or "tokyonight-night"
 	vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", {bg = "#1a1b26"})
-	vim.api.nvim_set_hl(0, "NormalFloat", {bg = "#1a1b26"})
+	--vim.api.nvim_set_hl(0, "Normal", {bg = "#1a1b26"})
+	--vim.api.nvim_set_hl(0, "NormalFloat", {bg = "#1a1b26"})
 end
 
-vim.cmd[[colorscheme tokyonight-night]]
 ColorEditor()
