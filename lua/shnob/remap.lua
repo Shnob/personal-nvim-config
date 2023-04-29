d1b5110bcb77fccc709aa53dc959dae46403e0a8
Shnob <jake.dombnz@gmail.com>
2023-04-29 14:42:24 +1200

    Finally set up lsp commands that work with jdtls.

--- lua/shnob/remap.lua
+++ lua/shnob/remap.lua
@@ -17,3 +17,18 @@ vim.keymap.set("n", "<leader>tp", vim.cmd.tabp)
 vim.keymap.set("n", "<leader>tc", vim.cmd.tabclose)
 
 vim.keymap.set("t", "<C-\\>", "<C-\\><C-n>");
+
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
+vim.keymap.set("n", "go", vim.lsp.buf.type_definition)
+vim.keymap.set("n", "gr", vim.lsp.buf.references)
+vim.keymap.set("n", "gs", vim.lsp.buf.signature_help)
+vim.keymap.set("n", "<F2>", vim.lsp.buf.rename)
+vim.keymap.set("n", "<F3>", vim.lsp.buf.format)
+vim.keymap.set("n", "<F4>", vim.lsp.buf.code_action)
+vim.keymap.set("n", "gl", vim.diagnostic.open_float)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
