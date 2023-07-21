6b3b8a6e1b161911a0f0509dc94f35cf84f15292
Shnob <jake.dombnz@gmail.com>
2023-07-21 15:20:50 +1200

    Folds are now preserved on write, and loaded on read

--- lua/shnob/set.lua
+++ lua/shnob/set.lua
@@ -29,6 +29,14 @@ vim.opt.cursorline = true
 
 -- For UFO
 vim.o.foldcolumn = '0' -- '0' is not bad
-vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+
+vim.cmd [[
+augroup remember_folds
+  autocmd!
+  autocmd BufWrite * mkview
+  autocmd BufRead * silent! loadview
+augroup END
+]]
