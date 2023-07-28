ff43e2a7de87ca71a4a2ca0c38f74c26e92f41e5
Shnob <jake.dombnz@gmail.com>
2023-07-28 14:15:57 +1200

    Removed Oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,13 +1,14 @@
-return {
-    "stevearc/oil.nvim",
-    lazy = false,
-    dependencies = {
-        { "nvim-tree/nvim-web-devicons" },
-    },
-    config = function()
-        require('oil').setup()
-    end,
-    keys = {
-        { "<leader>fo", "<cmd>Oil<cr>", { desc = "Open Oil" } },
-    }
-}
+return {}
+--return {
+--    "stevearc/oil.nvim",
+--    lazy = false,
+--    dependencies = {
+--        { "nvim-tree/nvim-web-devicons" },
+--    },
+--    config = function()
+--        require('oil').setup()
+--    end,
+--    keys = {
+--        { "<leader>fo", "<cmd>Oil<cr>", { desc = "Open Oil" } },
+--    }
+--}
