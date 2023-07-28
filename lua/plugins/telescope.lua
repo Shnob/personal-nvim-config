5cb01752482332e453b15a05457862ffc7594c9e
Shnob <jake.dombnz@gmail.com>
2023-07-28 22:06:04 +1200

    Added a couple lsp keybinds to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,5 +7,8 @@ return
     keys = {
         { '<leader>ff', "<cmd>Telescope find_files<cr>", { desc = "Tele: find files" } },
         { '<leader>fj', "<cmd>Telescope git_files<cr>",  { desc = "Tele: git files" } },
+        { '<leader>fr', "<cmd>Telescope lsp_references<cr>",  { desc = "Tele: Lsp references" } },
+        { '<leader>fd', "<cmd>Telescope lsp_definitions<cr>",  { desc = "Tele: Lsp definitions" } },
+        { '<leader>fm', "<cmd>Telescope lsp_implementations<cr>",  { desc = "Tele: Lsp implementations" } },
     }
 }
