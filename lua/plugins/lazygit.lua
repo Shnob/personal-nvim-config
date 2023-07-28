fd9dbb8fa60020fecb1e630e69801fa2c46abe6c
Shnob <jake.dombnz@gmail.com>
2023-07-29 00:09:25 +1200

    Added lazygit
    Btw I'm using lazygit inside of neovim for this commit

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,17 @@
+return {
+    "kdheepak/lazygit.nvim",
+
+    cmd = { 'LazyGit' },
+
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+
+    config = function()
+        Set_custom_powershell_options()
+    end,
+
+    keys = {
+        { '<leader>gg', "<cmd>LazyGit<cr>" },
+    }
+}
