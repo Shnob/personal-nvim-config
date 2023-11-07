dab251c2aaa61525395f41348859e13b599cc876
Jake Domb <jake.dombnz@gmail.com>
2023-11-07 22:18:55 +1300

    Changed colourscheme to spaceduck

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+    --"folke/tokyonight.nvim",
+    --config = function ()
+    --    vim.cmd.colorscheme("tokyonight-moon")
+    --end
+    
+    "pineapplegiant/spaceduck",
+    config = function ()
+        vim.cmd.colorscheme("spaceduck")
+    end
+}
