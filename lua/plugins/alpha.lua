5f1f18505f30c8378ac9a756a210d597745d0d57
Jake Domb <jake.dombnz@gmail.com>
2023-11-07 22:17:48 +1300

    Changed alpha splash icons

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -30,12 +30,12 @@ return {
             "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
         }
         dashboard.section.buttons.val = {
-            dashboard.button("n", "  New file", ":ene <BAR> startinsert <CR>"),
-            dashboard.button("e", "  Open netrw", ":Ex <CR>"),
+            dashboard.button("n", "  New file", ":ene <BAR> startinsert <CR>"),
+            dashboard.button("e", "󱏒  Open netrw", ":Ex <CR>"),
             dashboard.button("f", "󰮗  Find file", ":Telescope find_files<CR>"),
             dashboard.button("j", "  Git files", ":Telescope git_files<CR>"),
-            dashboard.button("r", "  Recent", ":Telescope oldfiles<CR>"),
-            dashboard.button("q", "  Quit NVIM", ":qa<CR>"),
+            dashboard.button("r", "󰔛  Recent", ":Telescope oldfiles<CR>"),
+            dashboard.button("q", "󰗼  Quit NVIM", ":qa<CR>"),
         }
         require 'alpha'.setup(dashboard.config)
     end,
