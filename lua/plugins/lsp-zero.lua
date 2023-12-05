0d1b648e4d0087e6af4a083260f06305aca7b068
Jake Domb <jake.dombnz@gmail.com>
2023-12-05 13:21:36 +1300

    Reworked lsp-zero

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -28,67 +28,32 @@ return {
                 luasnip.setup({})
                 require("luasnip.loaders.from_vscode").lazy_load()
 
-                vim.keymap.set({ 's', 'i' }, '<A-l>', function() if luasnip.jumpable(1) then luasnip.jump(1) end end,
+                vim.keymap.set({ 's', 'i' }, '<A-l>', function()
+                        if luasnip.jumpable(1) then luasnip.jump(1) end
+                    end,
                     { desc = 'LuaSnip: Next Placeholder' })
-                vim.keymap.set({ 's', 'i' }, '<A-h>', function() if luasnip.jumpable(-1) then luasnip.jump(-1) end end,
+                vim.keymap.set({ 's', 'i' }, '<A-h>', function()
+                        if luasnip.jumpable(-1) then luasnip.jump(-1) end
+                    end,
                     { desc = 'LuaSnip: Prev Placeholder' })
             end
         },
-
-        -- { 'folke/neodev.nvim' },
     },
     config = function()
-        -- require('neodev').setup({})
-
         local lsp_zero = require('lsp-zero')
-
-        require('mason').setup({})
-
-        lsp_zero.preset({
-            manage_nvim_cmp = {
-                --set_extra_mappings = true,
-            }
-        })
-
         local lspconfig = require('lspconfig')
 
-        lsp_zero.preset('recommended')
+        lsp_zero.on_attach(function(client, bufnr)
+            lsp_zero.default_keymaps({ buffer = bufnr })
+        end)
 
+        require('mason').setup({})
         require('mason-lspconfig').setup({
-            ensure_installed = {
-                'lua_ls',
-            },
+            ensure_installed = { 'lua_ls' },
             handlers = {
                 lsp_zero.default_setup,
-                --                lua_ls = function()
-                --                    local lua_opts = lsp_zero.nvim_lua_ls()
-                --
-                --                    for k, v in pairs(lua_opts.settings.Lua.workspace) do
-                --                        -- print(k .. " : ".. tostring(v))
-                --                    end
-                --                    lua_opts.settings.Lua.workspace.checkThirdParty = "Disable"
-                --
-                --                    require('lspconfig').lua_ls.setup(lua_opts)
-                --                end,
-            }
+            },
         })
-
-        local cmp = require('cmp')
-
-        cmp.setup({
-            mapping = cmp.mapping.preset.insert({
-            }),
-        })
-
-        -- lsp_zero.configure('gdscript', {
-        --     force_setup = true,                    -- because the LSP is global. Read more on lsp-zero docs about this.
-        --     single_file_support = false,
-        --     cmd = { 'ncat', '127.0.0.1', '6008' }, -- the important trick for Windows!
-        --     root_dir = require('lspconfig.util').root_pattern('project.godot', '.git'),
-        --     filetypes = { 'gd', 'gdscript', 'gdscript3' }
-        -- })
-
-        lsp_zero.setup()
 
         lsp_zero.set_sign_icons({
             error = "",
@@ -97,32 +62,11 @@ return {
             info = "",
         })
 
-        vim.diagnostic.config({
-            virtual_text = true,
-        })
-
         lspconfig.clangd.setup {
             cmd = {
                 "clangd",
                 "--offset-encoding=utf-16",
             }
         }
-
-        lspconfig.ocamllsp.setup {
-        }
-
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP: hover" })
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "LSP: goto definition" })
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "LSP: goto declaration" })
-        vim.keymap.set("n", "gm", vim.lsp.buf.implementation, { desc = "LSP: goto implementation" })
-        vim.keymap.set("n", "go", vim.lsp.buf.type_definition, { desc = "LSP: goto type definition" })
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "LSP: goto references" })
-        vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, { desc = "LSP: signature help" })
-        vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, { desc = "LSP: rename" })
-        vim.keymap.set("n", "<F3>", vim.lsp.buf.format, { desc = "LSP: format" })
-        vim.keymap.set("n", "<F4>", vim.lsp.buf.code_action, { desc = "LSP: code action" })
-        vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "Diag: open float" })
-        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Diag: goto prev" })
-        vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Diag: goto next" })
     end
 }
