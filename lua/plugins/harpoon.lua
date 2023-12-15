68a5eaeb569446ef82576f9da94bc687e901bf4d
Jake Domb <jake.dombnz@gmail.com>
2023-12-15 17:07:02 +1300

    Updated harpoon to harpoon2

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,18 +1,22 @@
 return
 {
     "ThePrimeagen/harpoon",
-    lazy = true,
+    -- lazy = true,
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        require('harpoon'):setup({ settings = { save_on_toggle = true } })
+    end,
     keys = {
-
-        { "<leader>ha", function () require("harpoon.mark").add_file() end,        { desc = "Harpoon: mark" } },
-        { "<leader>hh", function () require("harpoon.ui").toggle_quick_menu() end, { desc = "Harpoon: menu" } },
-        { "<leader>q",  function () require("harpoon.ui").nav_file(1) end,       { desc = "Harpoon: 1" } },
-        { "<leader>w",  function () require("harpoon.ui").nav_file(2) end,       { desc = "Harpoon: 2" } },
-        { "<leader>e",  function () require("harpoon.ui").nav_file(3) end,       { desc = "Harpoon: 3" } },
-        { "<leader>r",  function () require("harpoon.ui").nav_file(4) end,       { desc = "Harpoon: 4" } },
-        { "<leader>1",  function () require("harpoon.ui").nav_file(5) end,       { desc = "Harpoon: 5" } },
-        { "<leader>2",  function () require("harpoon.ui").nav_file(6) end,       { desc = "Harpoon: 6" } },
-        { "<leader>3",  function () require("harpoon.ui").nav_file(7) end,       { desc = "Harpoon: 7" } },
-        { "<leader>4",  function () require("harpoon.ui").nav_file(8) end,       { desc = "Harpoon: 8" } },
+        { "<leader>ha", function() require('harpoon'):list():append() end,                                 { desc = "Harpoon: mark" } },
+        { "<leader>hh", function() require('harpoon').ui:toggle_quick_menu(require('harpoon'):list()) end, { desc = "Harpoon: menu" } },
+        { "<leader>q",  function() require('harpoon'):list():select(1) end,                                { desc = "Harpoon: 1" } },
+        { "<leader>w",  function() require('harpoon'):list():select(2) end,                                { desc = "Harpoon: 2" } },
+        { "<leader>e",  function() require('harpoon'):list():select(3) end,                                { desc = "Harpoon: 3" } },
+        { "<leader>r",  function() require('harpoon'):list():select(4) end,                                { desc = "Harpoon: 4" } },
+        { "<leader>1",  function() require('harpoon'):list():select(5) end,                                { desc = "Harpoon: 5" } },
+        { "<leader>2",  function() require('harpoon'):list():select(6) end,                                { desc = "Harpoon: 6" } },
+        { "<leader>3",  function() require('harpoon'):list():select(7) end,                                { desc = "Harpoon: 7" } },
+        { "<leader>4",  function() require('harpoon'):list():select(8) end,                                { desc = "Harpoon: 8" } },
     }
 }
