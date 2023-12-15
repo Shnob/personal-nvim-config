a3e794e895f933bb009e6ce8bda11c3aac256fc9
Jake Domb <jake.dombnz@gmail.com>
2023-12-15 17:08:51 +1300

    Changed indent-blankline character

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -5,6 +5,7 @@ return
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
         require "ibl".setup {
+            indent = { char = "▏" }, -- ┆ ▏
             -- for example, context is off by default, use this to turn it on
 
             -- show_current_context = true,
