e4d453a1cd23e1360fa882bfd5e1d845a7c9a1c9
Jake Domb <jake.dombnz@gmail.com>
2024-01-06 01:18:22 +1300

    Hardtime.nvim added and configured
    The arrow keys have been enabled (they're on my home row) but they have been restricted (no spamming)

---
+++ lua/plugins/hardtime.lua
@@ -0,0 +1,17 @@
+return {
+    "m4xshen/hardtime.nvim",
+    opts = {
+        restricted_keys = {
+            ["<Up>"] = { "n", "x" },
+            ["<Down>"] = { "n", "x" },
+            ["<Left>"] = { "n", "x" },
+            ["<Right>"] = { "n", "x" },
+        },
+        disabled_keys = {
+            ["<Up>"] = {},
+            ["<Down>"] = {},
+            ["<Left>"] = {},
+            ["<Right>"] = {},
+        }
+    },
+}
