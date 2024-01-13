99d12e0936eb3bbe52a932116717cb0b15d99630
Shnob <jake.dombnz@gmail.com>
2024-01-13 13:15:21 +1300

    Enabled and configured nvim-treesitter-context's separator

--- lua/shnob/set.lua
+++ lua/shnob/set.lua
@@ -62,34 +62,41 @@ vim.api.nvim_create_autocmd("FileType", {
 --augroup END
 --]]
 
-vim.g.set_extra_colorscheme_settings = function()
+vim.g.set_extra_colorscheme_settings_transparent = function()
     vim.cmd [[
-            hi Folded ctermbg=NONE guibg=NONE
-            hi NormalFloat ctermbg=NONE guibg=NONE
-            hi FloatBorder ctermbg=NONE guibg=NONE
-            hi TelescopeNormal ctermbg=NONE guibg=NONE
-            hi TelescopeBorder ctermbg=NONE guibg=NONE
-            hi NotifyBackground ctermbg=NONE guibg=NONE
-            hi NotifyERRORBody ctermbg=NONE guibg=NONE
-            hi NotifyWARNBody ctermbg=NONE guibg=NONE
-            hi NotifyINFOBody ctermbg=NONE guibg=NONE
-            hi NotifyDEBUGBody ctermbg=NONE guibg=NONE
-            hi NotifyTRACEBody ctermbg=NONE guibg=NONE
-            hi NotifyERRORBorder ctermbg=NONE guibg=NONE
-            hi NotifyWARNBorder ctermbg=NONE guibg=NONE
-            hi NotifyINFOBorder ctermbg=NONE guibg=NONE
-            hi NotifyDEBUGBorder ctermbg=NONE guibg=NONE
-            hi NotifyTRACEBorder ctermbg=NONE guibg=NONE
-            hi TreeSitterContext ctermbg=NONE guibg=NONE
-            hi WinSeparator ctermbg=NONE guibg=NONE
-        ]]
+        hi Folded ctermbg=NONE guibg=NONE
+        hi NormalFloat ctermbg=NONE guibg=NONE
+        hi FloatBorder ctermbg=NONE guibg=NONE
+        hi TelescopeNormal ctermbg=NONE guibg=NONE
+        hi TelescopeBorder ctermbg=NONE guibg=NONE
+        hi NotifyBackground ctermbg=NONE guibg=NONE
+        hi NotifyERRORBody ctermbg=NONE guibg=NONE
+        hi NotifyWARNBody ctermbg=NONE guibg=NONE
+        hi NotifyINFOBody ctermbg=NONE guibg=NONE
+        hi NotifyDEBUGBody ctermbg=NONE guibg=NONE
+        hi NotifyTRACEBody ctermbg=NONE guibg=NONE
+        hi NotifyERRORBorder ctermbg=NONE guibg=NONE
+        hi NotifyWARNBorder ctermbg=NONE guibg=NONE
+        hi NotifyINFOBorder ctermbg=NONE guibg=NONE
+        hi NotifyDEBUGBorder ctermbg=NONE guibg=NONE
+        hi NotifyTRACEBorder ctermbg=NONE guibg=NONE
+        hi TreeSitterContext ctermbg=NONE guibg=NONE
+        hi WinSeparator ctermbg=NONE guibg=NONE
+    ]]
+end
+
+vim.g.set_extra_colorscheme_settings_all = function()
+    vim.cmd [[
+        hi! link TreeSitterContextSeparator LineNr
+    ]]
 end
 
 vim.api.nvim_create_autocmd("ColorScheme", {
     callback = function()
         if type(vim.g.transparent_enabled) == "boolean"
             and vim.g.transparent_enabled then
-            vim.g.set_extra_colorscheme_settings()
+            vim.g.set_extra_colorscheme_settings_transparent()
         end
+        vim.g.set_extra_colorscheme_settings_all()
     end
 })
