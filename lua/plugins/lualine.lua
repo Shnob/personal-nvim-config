adf4945e72173f71c3c21dd264f7badc09ae0294
Shnob <jake.dombnz@gmail.com>
2024-01-18 21:17:59 +1300

    Some components on tabline

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -17,7 +17,7 @@ return {
             violet   = '#a9a1e1',
             magenta  = '#c678dd',
             blue     = '#51afef',
-            red      = '#ec5f67',
+            red      = '#ff6a90',
         }
 
         local conditions = {
@@ -72,14 +72,19 @@ return {
             },
         }
 
-        -- Inserts a component in lualine_c at left section
-        local function ins_left(component)
+        -- Inserts a component in lualine
+        local function ins_lualine(component)
             table.insert(config.sections.lualine_c, component)
         end
 
-        -- Inserts a component in lualine_x at right section
-        local function ins_right(component)
-            table.insert(config.sections.lualine_x, component)
+        -- Inserts a component in tabline
+        local function ins_tabline(component)
+            table.insert(config.tabline.lualine_c, component)
+        end
+
+        -- Inserts a component in inactive lualine
+        local function ins_inactive(component)
+            table.insert(config.inactive_sections.lualine_c, component)
         end
 
         local color_func = function()
@@ -109,54 +114,70 @@ return {
             return { fg = mode_color[vim.fn.mode()] }
         end
 
-        -- ins_left {
-        --     function()
-        --         return '▊'
-        --     end,
-        --     color = { fg = colors.blue },          -- Sets highlighting of component
-        --     padding = { left = 0, right = 1 },     -- We don't need space before this
-        -- }
+        ins_lualine {
+            function()
+                return '▊'
+            end,
+            color = { fg = colors.blue },      -- Sets highlighting of component
+            padding = { left = 0, right = 1 }, -- We don't need space before this
+        }
 
-        -- ins_left {
-        --     -- mode component
-        --     function()
-        --         return ''
-        --     end,
-        --     color = color_func,
-        --     padding = { right = 1 },
-        -- }
+        ins_lualine {
+            -- mode component
+            -- neovim icon
+            function()
+                return ''
+            end,
+            color = color_func,
+            padding = { right = 1 },
+        }
 
-        -- ins_left {
-        --     -- filesize component
-        --     'filesize',
-        --     cond = conditions.buffer_not_empty,
-        -- }
-
-        ins_left {
+        ins_lualine {
             'filename',
             cond = conditions.buffer_not_empty,
             color = { fg = colors.magenta, gui = 'bold' },
         }
 
-        ins_left { 'location' }
+        ins_lualine {
+            'filetype',
+            cond = conditions.buffer_not_empty,
+            color = { fg = colors.fg, gui = 'bold' },
+        }
 
-        ins_left { 'progress', color = { fg = colors.fg, gui = 'bold' } }
+        ins_lualine {
+            -- filesize component
+            'filesize',
+            cond = conditions.buffer_not_empty,
+        }
+
+        -- Add components to right sections
+        ins_lualine {
+            'o:encoding',       -- option component same as &encoding in viml
+            fmt = string.upper, -- I'm not sure why it's upper case either ;)
+            cond = conditions.hide_in_width,
+            color = { fg = colors.green, gui = 'bold' },
+        }
+
+        ins_lualine {
+            'fileformat',
+            fmt = string.upper,
+            icons_enabled = false, -- tux pengiun icon
+            color = { fg = colors.green, gui = 'bold' },
+        }
+
+        ins_lualine { 'location' }
+
+        ins_lualine { 'progress', color = { fg = colors.fg } }
 
         -- -- Insert mid section. You can make any number of sections in neovim :)
         -- -- for lualine it's any number greater then 2
-        -- ins_left {
+        -- ins_lualine {
         --     function()
         --         return '%='
         --     end,
         -- }
 
-        ins_left {
-            'branch',
-            icon = '',
-            color = { fg = colors.violet, gui = 'bold' },
-        }
-
-        ins_left {
+        ins_lualine {
             -- Lsp server name .
             function()
                 local msg = '-'
@@ -177,7 +198,7 @@ return {
             color = { fg = '#ffffff', gui = 'bold' },
         }
 
-        ins_left {
+        ins_lualine {
             'diagnostics',
             sources = { 'nvim_diagnostic' },
             symbols = { error = ' ', warn = ' ', info = ' ' },
@@ -188,22 +209,39 @@ return {
             },
         }
 
-        -- -- Add components to right sections
-        -- ins_left {
-        --     'o:encoding', -- option component same as &encoding in viml
-        --     fmt = string.upper, -- I'm not sure why it's upper case either ;)
-        --     cond = conditions.hide_in_width,
-        --     color = { fg = colors.green, gui = 'bold' },
+        -- ins_lualine {
+        --     function()
+        --         return '▊'
+        --     end,
+        --     color = { fg = colors.blue },
+        --     padding = { left = 1 },
         -- }
 
-        -- ins_left {
-        --     'fileformat',
-        --     fmt = string.upper,
-        --     icons_enabled = true,
-        --     color = { fg = colors.green, gui = 'bold' },
-        -- }
+        --- TABLINE ---
 
-        ins_left {
+        ins_tabline {
+            function()
+                return '▊'
+            end,
+            color = { fg = colors.blue },      -- Sets highlighting of component
+            padding = { left = 0, right = 1 }, -- We don't need space before this
+        }
+
+        ins_tabline {
+            -- print working directory
+            function()
+                return vim.fn.fnamemodify(vim.fn.getcwd(), ':~')
+            end,
+            color = { fg = colors.fg },
+        }
+
+        ins_tabline {
+            'branch',
+            icon = '',
+            color = { fg = colors.violet, gui = 'bold' },
+        }
+
+        ins_tabline {
             'diff',
             -- Is it me or the symbol for modified us really weird
             symbols = { added = ' ', modified = ' ', removed = ' ' },
@@ -214,14 +252,6 @@ return {
             },
             cond = conditions.hide_in_width,
         }
-
-        -- ins_left {
-        --     function()
-        --         return '▊'
-        --     end,
-        --     color = { fg = colors.blue },
-        --     padding = { left = 1 },
-        -- }
 
         -- Now don't forget to initialize lualine
         lualine.setup(config)
