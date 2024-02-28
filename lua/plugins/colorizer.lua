3b12585e4696e4a928dae7338de2293f3575531a
Shnob <jake.dombnz@gmail.com>
2024-02-28 22:30:21 +1300

    Added RRGGBBAA colors to colorizer

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -2,11 +2,16 @@ return {
     "norcalli/nvim-colorizer.lua",
     lazy = true,
 
+    config = function()
+        require("colorizer").setup(
+            {},
+            { RRGGBBAA = true }
+        )
+    end,
+
     cmd = { "ColorizerToggle", "ColorizerAttachToBuffer", "ColorizerReloadAllBuffers", "ColorizerDetachFromBuffer" },
 
     keys = {
         { "<leader>c", "<cmd>ColorizerToggle<CR>", { noremap = true, silent = true } },
     },
-
-    opts = {},
 }
