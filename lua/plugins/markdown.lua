38d7015e67e98ffd74bb182ea49b93b77ad344ee
Shnob <jake.dombnz@gmail.com>
2024-12-10 12:47:35 +1300

    Added markdown.nvim

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,20 @@
+return {
+    'MeanderingProgrammer/markdown.nvim',
+    main = "render-markdown",
+    opts = {
+        sign = {
+            enabled = false,
+        },
+        heading = {
+            backgrounds = { "none", "none", "none", "none", "none", "none", },
+        },
+        code = {
+            highlight = "none",
+            highlight_inline = "none",
+            border = 'thick',
+        }
+    },
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+}
