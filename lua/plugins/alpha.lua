32c25331a69ae8aa58764f7c304391ecc5fb2c84
Shnob <jake.dombnz@gmail.com>
2025-03-26 19:44:38 +1300

    Allowed Telescope's find_files to see ignored files. Updated Alpha buttons to reflect this.

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -16,8 +16,10 @@ return {
         dashboard.section.buttons.val = {
             dashboard.button("n", "  New file", ":ene <CR>"),
             dashboard.button("e", "󱏒  Open netrw", ":Ex <CR>"),
-            dashboard.button("f", "󰮗  Find file", ":Telescope find_files<CR>"),
-            dashboard.button("j", "  Git files", ":Telescope git_files<CR>"),
+            dashboard.button("f", "󰮗  Find file",
+                function() require("telescope.builtin").find_files({ no_ignore = true, no_ignore_parent = true }) end),
+            dashboard.button("j", "  Git files",
+                function() require("telescope.builtin").git_files() end),
             dashboard.button("r", "󰔛  Recent", ":Telescope oldfiles<CR>"),
             dashboard.button("q", "󰗼  Quit NVIM", ":qa<CR>"),
         }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,18 +8,26 @@ return
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
     keys = {
-        { '<leader>ff', "<cmd>Telescope find_files<cr>",                                       { desc = "Tele: find files" } },
-        { '<leader>fh', function() require("telescope.builtin").find_files({ hidden = true }) end, { desc = "Tele: find files (hidden)" } },
-        { '<leader>fj', "<cmd>Telescope git_files<cr>",                                        { desc = "Tele: git files" } },
-        { '<leader>fr', "<cmd>Telescope lsp_references<cr>",                                   { desc = "Tele: Lsp references" } },
-        { '<leader>fd', "<cmd>Telescope lsp_definitions<cr>",                                  { desc = "Tele: Lsp definitions" } },
-        { '<leader>fm', "<cmd>Telescope lsp_implementations<cr>",                              { desc = "Tele: Lsp implementations" } },
-        { '<leader>fl', "<cmd>Telescope diagnostics<cr>",                                      { desc = "Tele: Lsp diagnostics" } },
-        { '<leader>fk', "<cmd>Telescope buffers<cr>",                                          { desc = "Tele: buffers" } },
-        { '<leader>fc', "<cmd>Telescope colorscheme<cr>",                                      { desc = "Tele: colorscheme" } },
-        { '<leader>fz', "<cmd>Telescope spell_suggest<cr>",                                    { desc = "Tele: colorscheme" } },
-        { '<leader>fo', "<cmd>Telescope oldfiles<cr>",                                         { desc = "Tele: oldfiles" } },
-        { '<leader>fg', "<cmd>Telescope live_grep<cr>",                                        { desc = "Tele: live grep" } },
+        {
+            '<leader>ff',
+            function() require("telescope.builtin").find_files({ no_ignore = true, no_ignore_parent = true }) end,
+            { desc = "Tele: find files" }
+        },
+        {
+            '<leader>fh',
+            function() require("telescope.builtin").find_files({ no_ignore = true, no_ignore_parent = true, hidden = true }) end,
+            { desc = "Tele: find files (hidden)" }
+        },
+        { '<leader>fj', "<cmd>Telescope git_files<cr>",           { desc = "Tele: git files" } },
+        { '<leader>fr', "<cmd>Telescope lsp_references<cr>",      { desc = "Tele: Lsp references" } },
+        { '<leader>fd', "<cmd>Telescope lsp_definitions<cr>",     { desc = "Tele: Lsp definitions" } },
+        { '<leader>fm', "<cmd>Telescope lsp_implementations<cr>", { desc = "Tele: Lsp implementations" } },
+        { '<leader>fl', "<cmd>Telescope diagnostics<cr>",         { desc = "Tele: Lsp diagnostics" } },
+        { '<leader>fk', "<cmd>Telescope buffers<cr>",             { desc = "Tele: buffers" } },
+        { '<leader>fc', "<cmd>Telescope colorscheme<cr>",         { desc = "Tele: colorscheme" } },
+        { '<leader>fz', "<cmd>Telescope spell_suggest<cr>",       { desc = "Tele: colorscheme" } },
+        { '<leader>fo', "<cmd>Telescope oldfiles<cr>",            { desc = "Tele: oldfiles" } },
+        { '<leader>fg', "<cmd>Telescope live_grep<cr>",           { desc = "Tele: live grep" } },
     },
     opts = {
         defaults = {
