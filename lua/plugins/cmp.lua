b9c225ebdf80c19e986c5ce3473baf329a4ed9fb
Shnob <jake.dombnz@gmail.com>
2025-05-24 13:52:11 +1200

    Moved cmp hl data to colorscheme-settings.lua
    Last commit mentions that LspKind is broken. This is not true.
    This commit fixes the problem I thought LspKind was causing.

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -98,56 +98,5 @@ return {
                 end,
             },
         })
-
-        local field_property_event = "#B5585F"
-        local text_enum_keyword = "#9FBD73"
-        local constant_constructor_reference = "#D4BB6C"
-        local function_struct_class_module_operator = "#A377BF"
-        local variable_file = "#7E8294"
-        local unit_snippet_folder = "#D4A959"
-        local method_value_member = "#6C8ED4"
-        local interface_color_typeparameter = "#58B5A8"
-
-        -- Customization for Pmenu
-        vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#282C34", fg = "NONE" })
-        vim.api.nvim_set_hl(0, "Pmenu", { fg = "#C5CDD9", bg = "NONE" })
-
-        vim.api.nvim_set_hl(0, "CmpItemAbbrDeprecated", { fg = "#7E8294", bg = "NONE", strikethrough = true })
-        vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { fg = "#82AAFF", bg = "NONE", bold = true })
-        vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { fg = "#82AAFF", bg = "NONE", bold = true })
-        vim.api.nvim_set_hl(0, "CmpItemMenu", { fg = "#C792EA", bg = "NONE", italic = true })
-
-        vim.api.nvim_set_hl(0, "CmpItemKindField", { fg = field_property_event, bg = "NONE" })
-        vim.api.nvim_set_hl(0, "CmpItemKindProperty", { fg = field_property_event, bg = "NONE" })
-        vim.api.nvim_set_hl(0, "CmpItemKindEvent", { fg = field_property_event, bg = "NONE" })
-
-        vim.api.nvim_set_hl(0, "CmpItemKindText", { fg = text_enum_keyword, bg = "NONE" })
-        vim.api.nvim_set_hl(0, "CmpItemKindEnum", { fg = text_enum_keyword, bg = "NONE" })
-        vim.api.nvim_set_hl(0, "CmpItemKindKeyword", { fg = text_enum_keyword, bg = "NONE" })
-
-        vim.api.nvim_set_hl(0, "CmpItemKindConstant", { fg = constant_constructor_reference, bg = "NONE" })
-        vim.api.nvim_set_hl(0, "CmpItemKindConstructor", { fg = constant_constructor_reference, bg = "NONE" })
-        vim.api.nvim_set_hl(0, "CmpItemKindReference", { fg = constant_constructor_reference, bg = "NONE" })
-
-        vim.api.nvim_set_hl(0, "CmpItemKindFunction", { fg = function_struct_class_module_operator, bg = "NONE" })
-        vim.api.nvim_set_hl(0, "CmpItemKindStruct", { fg = function_struct_class_module_operator, bg = "NONE" })
-        vim.api.nvim_set_hl(0, "CmpItemKindClass", { fg = function_struct_class_module_operator, bg = "NONE" })
-        vim.api.nvim_set_hl(0, "CmpItemKindModule", { fg = function_struct_class_module_operator, bg = "NONE" })
-        vim.api.nvim_set_hl(0, "CmpItemKindOperator", { fg = function_struct_class_module_operator, bg = "NONE" })
-
-        vim.api.nvim_set_hl(0, "CmpItemKindVariable", { fg = variable_file, bg = "NONE" })
-        vim.api.nvim_set_hl(0, "CmpItemKindFile", { fg = variable_file, bg = "NONE" })
-
-        vim.api.nvim_set_hl(0, "CmpItemKindUnit", { fg = unit_snippet_folder, bg = "NONE" })
-        vim.api.nvim_set_hl(0, "CmpItemKindSnippet", { fg = unit_snippet_folder, bg = "NONE" })
-        vim.api.nvim_set_hl(0, "CmpItemKindFolder", { fg = unit_snippet_folder, bg = "NONE" })
-
-        vim.api.nvim_set_hl(0, "CmpItemKindMethod", { fg = method_value_member, bg = "NONE" })
-        vim.api.nvim_set_hl(0, "CmpItemKindValue", { fg = method_value_member, bg = "NONE" })
-        vim.api.nvim_set_hl(0, "CmpItemKindEnumMember", { fg = method_value_member, bg = "NONE" })
-
-        vim.api.nvim_set_hl(0, "CmpItemKindInterface", { fg = interface_color_typeparameter, bg = "NONE" })
-        vim.api.nvim_set_hl(0, "CmpItemKindColor", { fg = interface_color_typeparameter, bg = "NONE" })
-        vim.api.nvim_set_hl(0, "CmpItemKindTypeParameter", { fg = interface_color_typeparameter, bg = "NONE" })
     end,
 }
